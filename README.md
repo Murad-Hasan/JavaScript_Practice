963576e069777fd3e665e20c689d838c5a29dffb
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-21 20:08:13 +0600

    readme add

---
+++ README.md
@@ -0,0 +1,3 @@
+For instant result use Quokka extension in vscode
+Fast install extension then click Ctrl + Shift + p
+then search quokka start on current file.
--- Strings.js
+++ Strings.js
@@ -27,13 +27,30 @@ const res5 = str.includes("murad");
 const res6 = str.includes("World", 6);
 const res7 = str.includes("World", 7);
 console.log(res6);
-console.log("res7", res7)
+console.log("res7", res7);
 console.log("res5", res5);
 
 //indexOf
 const res8 = str.indexOf("World");
 const res9 = str.indexOf("murad");
-console.log("res9", res9) //noting found
+console.log("res9", res9); //noting found
 console.log("res8", res8);
 
+//lastIndexOf
+const res10 = str.lastIndexOf("World"); // it searches from the end but it's position count from the start
+console.log("res10", res10);
 
+//match
+const string = "metaverse world";
+const res11 = string.match("world");
+console.log(res11);
+
+const string1 = "the rain in spin stays mainly in the plain";
+const res12 = string1.match(/ain/g);
+console.log(res12);
+
+//repeat
+const res13 = str.repeat(3);
+console.log("res13", res13);
+
+// replace
