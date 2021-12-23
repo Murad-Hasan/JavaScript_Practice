e83b2973d84e5e4d9e07d29fb54c0ef93de28256
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-23 18:04:59 +0600

    array.js

--- Array.js
+++ Array.js
@@ -10,6 +10,8 @@ let cities = [
   { name: "Chicago", temperature: 59.0, population: 20 },
 ];
 
+//By for loop
+
 let bigCities = [];
 let lowTamper = [];
 let smallCities = [];
@@ -25,6 +27,22 @@ for (let i = 0; i < cities.length; i++) {
   }
 }
 
-console.log(bigCities);
-console.log(lowTamper);
-console.log(smallCities);
+// console.log(bigCities);
+// console.log(lowTamper);
+// console.log(smallCities);
+
+
+//by filter method
+
+// let bigCities = cities.filter(function (e) {
+//     return e.population > 3000000;
+// });
+
+//short hand ==>
+
+let biggerCities = cities.filter(city => city.population >= 100 && city.temperature >= 60);
+
+// console.log(biggerCities)
+
+
+cities.filter(city => city.population < 100).sort((a, b) => a.population - b.population).map(city => console.log(city.name + ":" + city.population));
