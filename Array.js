6ee04a8c72207e1976e856f6766f8fde23260160
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-23 16:50:08 +0600

    array.js

--- Array.js
+++ Array.js
@@ -1,25 +1,30 @@
 // array filter
 
 let cities = [
-    {name: 'Los Angeles', temperature: 60.0, population: 100},
-    {name: 'Atlanta', temperature: 52.0, population: 120},
-    {name: 'Detroit', temperature: 48.0, population: 60},
-    {name: 'New York', temperature: 80.0, population: 50},
-    {name: 'Miami', temperature: 73.0, population: 150},
-    {name: 'Boston', temperature: 59.0, population: 35},
-    {name: 'Chicago', temperature: 59.0, population: 20}
-]
+  { name: "Los Angeles", temperature: 60.0, population: 100 },
+  { name: "Atlanta", temperature: 52.0, population: 120 },
+  { name: "Detroit", temperature: 48.0, population: 60 },
+  { name: "New York", temperature: 80.0, population: 50 },
+  { name: "Miami", temperature: 73.0, population: 150 },
+  { name: "Boston", temperature: 59.0, population: 35 },
+  { name: "Chicago", temperature: 59.0, population: 20 },
+];
 
 let bigCities = [];
+let lowTamper = [];
 let smallCities = [];
 
 for (let i = 0; i < cities.length; i++) {
-    if (cities[i].population >= 100) {
-        bigCities.push(cities[i]);
-    }else{
-        smallCities.push(cities[i]);
-    }
+  if (cities[i].population >= 100 && cities[i].temperature >= 60) {
+    bigCities.push(cities[i]);
+  }
+  if (cities[i].population >= 100  && !(cities[i].temperature >= 60)) {
+    lowTamper.push(cities[i]);
+  } else {
+    smallCities.push(cities[i]);
+  }
 }
 
 console.log(bigCities);
+console.log(lowTamper);
 console.log(smallCities);
