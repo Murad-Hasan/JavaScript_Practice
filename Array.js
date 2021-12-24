40afa64784de9388402b79e3ad94fab59d24f9f0
Murad-Hasan <muradhasan.7845@gmail.com>
2021-12-24 22:24:48 +0600

    add

--- Array.js
+++ Array.js
@@ -61,4 +61,18 @@ let a = [1, 2, 3];
 let b = [0, ...a, 4];
 console.log(b); // [0, 1, 2, 3, 4]
 
+let digits = [..."123456789ABCDEFGH"]
+console.log(digits); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+// creat a new array from an array without reapted values
+let letters = [..."Hello World"]
+let notRepeatLetters = [...new Set(letters)]
+
+console.log(letters); // ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+console.log(notRepeatLetters); // ['H', 'e', 'l', 'o', 'W', 'r', 'd']
+
+let numbers = [1, 2, 3, 4, 5, 6,6, 7, 7, 8, 9, 10];
+let notRepeadNumbers = [...new Set(numbers)];
+console.log(numbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(notRepeadNumbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
