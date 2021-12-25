8b154c09e8d1df58521bdf47ec5e4ccda31f79a6
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-25 20:46:04 +0600

    array.js

--- Array.js
+++ Array.js
@@ -20,7 +20,7 @@ for (let i = 0; i < cities.length; i++) {
   if (cities[i].population >= 100 && cities[i].temperature >= 60) {
     bigCities.push(cities[i]);
   }
-  if (cities[i].population >= 100  && !(cities[i].temperature >= 60)) {
+  if (cities[i].population >= 100 && !(cities[i].temperature >= 60)) {
     lowTamper.push(cities[i]);
   } else {
     smallCities.push(cities[i]);
@@ -31,7 +31,6 @@ for (let i = 0; i < cities.length; i++) {
 // console.log(lowTamper);
 // console.log(smallCities);
 
-
 //by filter method
 
 // let bigCities = cities.filter(function (e) {
@@ -40,16 +39,20 @@ for (let i = 0; i < cities.length; i++) {
 
 //short hand ==>
 
-let biggerCities = cities.filter(city => city.population >= 100 && city.temperature >= 60);
+let biggerCities = cities.filter(
+  (city) => city.population >= 100 && city.temperature >= 60
+);
 
 // console.log(biggerCities)
 
-
-cities.filter(city => city.population < 100).sort((a, b) => a.population - b.population).map(city => console.log(city.name + ":" + city.population));
+cities
+  .filter((city) => city.population < 100)
+  .sort((a, b) => a.population - b.population)
+  .map((city) => console.log(city.name + ":" + city.population));
 
 console.log("hello world");
 
-let fruits = []
+let fruits = [];
 fruits[9999] = 5;
 fruits.age = 9;
 
@@ -61,40 +64,39 @@ let a1 = [1, 2, 3];
 let b1 = [0, ...a1, 4];
 console.log(b1); // [0, 1, 2, 3, 4]
 
-let digits = [..."123456789ABCDEFGH"]
+let digits = [..."123456789ABCDEFGH"];
 console.log(digits); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G']
 
 // creat a new array from an array without reapted values
-let letters = [..."Hello World"] // ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
-let notRepeatLetters = [...new Set(letters)]  // ["H", "e", "l", "o", "W", "r", "d"]
+let letters = [..."Hello World"]; // ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
+let notRepeatLetters = [...new Set(letters)]; // ["H", "e", "l", "o", "W", "r", "d"]
 
 console.log(letters); // ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
 console.log(notRepeatLetters); // ['H', 'e', 'l', 'o', 'W', 'r', 'd']
 
-let numbers = [1, 2, 3, 4, 5, 6,6, 7, 7, 8, 9, 10];
+let numbers = [1, 2, 3, 4, 5, 6, 6, 7, 7, 8, 9, 10];
 let notRepeatedNumbers = [...new Set(numbers)];
 console.log(numbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(notRepeatedNumbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-//array of 
+//array of
 Array.of(1, 2, 3); // [1, 2, 3]
 Array.of(1); // [1]
 Array.of(); // []
 
 //array from
 let array = Array.from("Hello World");
-console.log(array)// ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
-
+console.log(array); // ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
 
 // Reading and writing of a array
 
 let a = ["world"];
-let value = a[0] // "world"
+let value = a[0]; // "world"
 a[1] = 3.14;
 let i = 2;
 a[i] = 3;
 a[i + 1] = "hellol";
-a[a[i]] = a[0]; // --> bujhi nai asole ki hoise akhane 
+a[a[i]] = a[0]; // --> bujhi nai asole ki hoise akhane
 console.log(a[a[i]]);
 console.log(a); // ["world", 3.14, 3, "hellol", "world"]
 
@@ -102,12 +104,12 @@ console.log(a); // ["world", 3.14, 3, "hellol", "world"]
 
 let letters1 = [..."Hello World"];
 let string = "";
-for (let letter of letters1){
+for (let letter of letters1) {
   string = string + letter; //string += letter;
 }
 console.log(letters1, string); // "Hello World"
 
-//if we want to use a for/of loop for an array and need to know the index of each array element 
+//if we want to use a for/of loop for an array and need to know the index of each array element
 
 // let everyother = "";
 // for (let [index, letter] of letters1.entries()){
@@ -122,10 +124,29 @@ let vowels = "";
 //   if(/[aeiou]/.test(letter)) vowels += letter;
 // }
 
-
 //or
 
-for (const letter of letters1){
-  if(/[aeiou]/.test(letter)) vowels += letter
+for (const letter of letters1) {
+  if (/[aeiou]/.test(letter)) vowels += letter;
 }
 console.log(vowels);
+
+//array methods
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+data.forEach((value) => (sum += value));
+
+console.log("sum", sum, "Array", data);
+
+data.forEach((value, index, array) => {
+  array[index] = value * 2;
+});
+
+console.log(data) // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+data.forEach(function (v , i , a) {
+  a[i] = v * 3; 
+})
+
+console.log(data); // [6, 12, 18, 24, 30, 36, 42, 48, 54, 60]
